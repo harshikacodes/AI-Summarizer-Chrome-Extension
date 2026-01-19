189169f8a8fff93cfc1b153648b7e50420a0182c
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-01-19 10:31:28 +0530

    refactor background.js

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
+// it will only run once when the extension is istalled or updated
 chrome.runtime.onInstalled.addListener(() => {
+    // here i am checking if the Gemini API key is already saved
     chrome.storage.sync.get(["geminiApiKey"], (result) => {
+        // if the API key is missing, open the options page
+        // so the user can add it
         if(!result.geminiApiKey){
             chrome.tabs.create({url: "options.html"});
         }
